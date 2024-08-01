b7256b9f49aa1b84de19e017da5f6a0e565fb498
frederic smée <ssdfred@yahoo.fr>
2024-08-01 17:36:17 +0200

    chore: Add book-edit component and template

---
+++ src/app/components/books/book-edit/book-edit.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { BooksService } from '../../../services/books.service';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { BookInterface } from '../../../interfaces/book.interface';
+
+@Component({
+  selector: 'digi-book-edit',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule],
+  templateUrl: './book-edit.component.html',
+  styleUrl: './book-edit.component.scss'
+})
+export class EditBookComponent implements OnInit {
+
+
+  bookForm = new FormGroup({
+    title: new FormControl(''),
+    auteur: new FormControl(''),
+    description: new FormControl(''),
+
+  });
+
+  @Input() id!: string;
+  books: Observable<BookInterface[]>;
+  book: BookInterface | undefined;
+  constructor(private bookService: BooksService, private router: Router) {
+    this.books = this.bookService.one(this.id);
+    this.books.subscribe((book) => {
+      this.book = book[0];
+    });
+  }
+  ngOnInit(): void {
+    this.bookForm.setValue({ title: this.book!.titre, auteur: this.book!.auteur, description: this.book!.description });
+  }
+
+
+  edit() {
+    if (this.bookForm.value.title && this.bookForm.value.auteur && this.bookForm.value.description) {
+      const updatedBook = {
+        title: this.bookForm.value.title,
+        auteur: this.bookForm.value.auteur,
+        description: this.bookForm.value.description,
+      }
+      this.bookService.update(this.id, updatedBook);
+      this.router.navigate(['/books']);
+    }
+  }
+}
