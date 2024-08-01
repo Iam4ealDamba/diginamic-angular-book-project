17b64bb333b28613cb70d426c497150ecc8acc46
Iam4ealDamba <prayforsouls720@gmail.com>
2024-08-01 14:25:26 +0200

    update

---
+++ src/app/components/books/books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'digi-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.scss'
+})
+export class BooksComponent {
+
+}
