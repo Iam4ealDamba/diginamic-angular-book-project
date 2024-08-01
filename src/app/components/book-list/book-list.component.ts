658c8c930a885548c529198ec2758464486a3546
Iam4ealDamba <prayforsouls720@gmail.com>
2024-08-01 16:33:32 +0200

    ajout book-add

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -2,28 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { BookInterface } from '../../interfaces/book.interface';
 import { BooksService } from '../../services/books.service';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'digi-book-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.scss'
+  styleUrl: './book-list.component.scss',
 })
-export class BookListComponent  implements OnInit {
+export class BookListComponent implements OnInit {
+  books: Observable<BookInterface[]>;
 
-  books: BookInterface[] = [];
-
-  constructor(private bookService: BooksService) { }
+  constructor(private bookService: BooksService) {
+    this.books = this.bookService.all();
+  }
 
   ngOnInit(): void {
-    this.bookService.all().subscribe((books) => {
-      this.books = books;
-    });
+    this.books = this.bookService.all();
   }
   deleteBook(id: string) {
     this.bookService.delete(id).subscribe(() => {
-      this.books = this.books.filter((book) => book.id !== id);
+      this.books = this.bookService.all();
     });
   }
-
 }
