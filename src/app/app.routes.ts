0e159505db90e20db96b91d150e45a6684688d91
Iam4ealDamba <prayforsouls720@gmail.com>
2024-08-02 17:13:21 +0200

    last

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookAddComponent } from './components/books/book-add/book-add.component';
 import { EditBookComponent } from './components/books/book-edit/book-edit.component';
+import { BookDetailComponent } from './components/books/book-detail/book-detail.component';
 export const routes: Routes = [
   { path: '', component: BookListComponent },
   { path: 'add', component: BookAddComponent },
   { path: 'edit/:id', component: EditBookComponent },
-  // { path: 'detail/:id', component: BookDetailComponent },
+  { path: 'detail/:id', component: BookDetailComponent },
 ];
